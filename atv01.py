d2b31e2667e63b314303643ef7082dba70e2db93
MatheusSdeO <matheustoccodeoliveira@gmail.com>
2025-04-21 14:31:41 -0300

    Versão limpa com Dockerfile e atv01.py

---
+++ atv01.py
@@ -0,0 +1,8 @@
+import time
+
+print("🚀 Aplicação Python rodando dentro do container Docker!")
+
+# Mantém o container ativo por um tempo
+while True:
+    print("⏳ Container ainda está rodando...")
+    time.sleep(5)
