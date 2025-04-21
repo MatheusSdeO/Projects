e92c92e92413462dbaea6436784c5901a33338ff
MatheusSdeO <matheustoccodeoliveira@gmail.com>
2025-04-21 15:16:42 -0300

    Remove loop infinito e limita tempo de execução

--- atv01.py
+++ atv01.py
@@ -2,7 +2,9 @@ import time
 
 print("🚀 Aplicação Python rodando dentro do container Docker!")
 
-# Mantém o container ativo por um tempo
-while True:
+# Mantém o container ativo por 30 segundos
+for _ in range(6):  # 6 iterações de 5 segundos
     print("⏳ Container ainda está rodando...")
     time.sleep(5)
+
+print("⏳ O container terminou de rodar.")
